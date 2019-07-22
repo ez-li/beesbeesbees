aadd0eac8e6b943dac75a25ef6b2e64d52b2e6aa
ez-li <eileenli.sf@gmail.com>
2019-07-22 12:21:38 -0700

    Completed beesbeesbees

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,18 @@
-var HoneyMakerBee = function() {
+var HoneyMakerBee = function()
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype.eat = function() {
+  _.extend(this, Grub.prototype);
+}
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot ++;
+}
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot --;
+}
--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,20 @@
 var RetiredForagerBee = function() {
+  Bee.call(this);
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
 
+RetiredForagerBee.prototype.forage = function() {
+  return 'I am too old, let me play cards instead';
+}
+
+RetiredForagerBee.prototype.eat = function() {
+  _.extend(this, grub);
+}
+
+RetiredForagerBee.prototype.gamble = function(treasure) {
+  this.treasureChest.push(treasure);
+}
